d713e72da3e6d61eae8e31acc62239f1556e6431
The_Scientist777 <pmrojas195@gmail.com>
2023-11-08 10:51:51 -0600

    Made cool image transition

--- index_script.js
+++ index_script.js
@@ -1,7 +1,14 @@
 function showPopup() {
   alert("Someone is knocking on the door!");
-  // Redirect to "alibi.html" after showing the popup
-  window.location.href = "alibi.html";
+
+  // Set the background image using JavaScript
+  document.body.style.backgroundImage = "url('Knocking on door.jpg')";
+
+  // Wait for 2 seconds and reset the background image
+  setTimeout(() => {
+    document.body.style.backgroundImage = "url('Index background.jpg')";
+    window.location.href = "alibi.html";
+  }, 2000); // 2,000 milliseconds = 2 seconds
 }
 
 function showNote() {
